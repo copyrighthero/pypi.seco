a461a6198bd82836c8caaff3d94bc93173a5701f
Hansheng Zhao <zhaohans@msu.edu>
2018-03-18 21:02:23 -0400

    modified | modified setup.py and now uses specific imports for metadata.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from codecs import open
 from os import path
 
 # import library for metadata
-import seco
+from seco import __author__, __license__, __version__
 
 # project absolute directory
 DIRECTORY = path.abspath(path.dirname(__file__))
@@ -26,14 +26,14 @@ with open(
 # project setup parameters
 setup(
   name = 'SeCo',
-  version = seco.__version__,
+  version = __version__,
   description = 'Python data serialization and compression made easy.',
   long_description = PROJECT_README,
   url = 'https://www.github.com/copyrighthero/SeCo',
   download_url = 'https://www.github.com/copyrighthero/SeCo',
-  author = seco.__author__,
+  author = __author__,
   author_email = 'copyrighthero@gmail.com',
-  license = seco.__license__,
+  license = __license__,
   classifiers = (
     'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Developers',
